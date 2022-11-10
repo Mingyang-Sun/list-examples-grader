3f4cd1d6211de9e5d45640a1a35d0c931ecd408c
Mingyang-Sun <mingyangs0301@gmail.com>
2022-11-10 12:46:12 -0800

    lab 7

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,43 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class TestListExamples {
-  // Write your grading tests here!
+import java.nio.file.DirectoryStream.Filter;
+import java.util.ArrayList;
+import java.util.List;
+
+interface StringChecker { boolean checkString(String s); }
+
+class LengthChecker implements StringChecker
+{
+  public boolean checkString(String s)
+  {
+    if (s.length() > 1)
+    {
+      return true;
+    }
+    return false;
+  }
 }
+
+public class TestListExamples {
+    // Write your grading tests here!
+    @Test
+    public void testFilter() {
+        StringChecker sc = new LengthChecker();
+
+        ArrayList<String> input = new ArrayList<String>();
+        input.add("a");
+        input.add("apple");
+        input.add("b");
+        input.add("banana");
+        input.add("c");
+        input.add("carrot");
+
+        ArrayList<String> expect = input;
+        expect.remove("a");
+        expect.remove("b");
+        expect.remove("c");
+
+        assertArrayEquals(expect.toArray(), ListExamples.filter(input, sc).toArray());
+    }
+  }
--- grade.sh
+++ grade.sh
@@ -5,3 +5,17 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+cp TestListExamples.java student-submission
+cp -R lib student-submission
+
+cd student-submission
+
+if [[ -e ListExamples.java ]]
+then
+  javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> error.txt
+  java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
+  echo error.txt output.txt
+else
+  echo "wrong file"
+  exit 1
+fi
