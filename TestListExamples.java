91c593b7944d60327664a9a42975e3f998c1b9dc
Mingyang-Sun <mingyangs0301@gmail.com>
2022-11-27 20:54:07 -0800

    done

--- TestListExamples.java
+++ TestListExamples.java
@@ -3,14 +3,12 @@ import org.junit.*;
 
 import java.nio.file.DirectoryStream.Filter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-class LengthChecker implements StringChecker
-{
-  public boolean checkString(String s)
-  {
-    if (s.length() > 1)
-    {
+class LengthChecker implements StringChecker {
+  public boolean checkString(String s) {
+    if (s.length() > 1) {
       return true;
     }
     return false;
@@ -18,24 +16,47 @@ class LengthChecker implements StringChecker
 }
 
 public class TestListExamples {
-    // Write your grading tests here!
-    @Test
-    public void testFilter() {
-        StringChecker sc = new LengthChecker();
+  // Write your grading tests here!
+  @Test
+  public void testFilter1() {
+    StringChecker sc = new LengthChecker();
 
-        ArrayList<String> input = new ArrayList<String>();
-        input.add("a");
-        input.add("apple");
-        input.add("b");
-        input.add("banana");
-        input.add("c");
-        input.add("carrot");
+    ArrayList<String> input = new ArrayList<String>();
+    input.add("a");
+    input.add("apple");
+    input.add("b");
+    input.add("banana");
+    input.add("c");
+    input.add("carrot");
 
-        ArrayList<String> expect = input;
-        expect.remove("a");
-        expect.remove("b");
-        expect.remove("c");
+    ArrayList<String> expect = input;
+    expect.remove("a");
+    expect.remove("b");
+    expect.remove("c");
 
-        assertArrayEquals(expect.toArray(), ListExamples.filter(input, sc).toArray());
-    }
+    assertArrayEquals(expect.toArray(), ListExamples.filter(input, sc).toArray());
   }
+
+  @Test
+  public void testMerge1() {
+    List<String> list1 = new ArrayList<String>();
+    List<String> list2 = new ArrayList<String>();
+    list1.add("a");
+    list1.add("b");
+    list1.add("c");
+    list2.add("d");
+    list2.add("e");
+    list2.add("f");
+    List<String> list3 = ListExamples.merge(list1, list2);
+    String[] expected = { "a", "b", "c", "d", "e", "f" };
+    assertArrayEquals(expected, list3.toArray());
+  }
+
+  @Test
+  public void testMerge2(){
+    ArrayList<String> input1 = new ArrayList<>(Arrays.asList("a"));
+    ArrayList<String> input2 = new ArrayList<>(Arrays.asList("b", "c", "d"));
+    ArrayList<String> output = new ArrayList<>(Arrays.asList("a", "b", "c", "d"));
+    Assert.assertEquals(output, ListExamples.merge(input1, input2));
+  }
+}
--- grade.sh
+++ grade.sh
@@ -1,53 +1,56 @@
 # Create your grading script here
 
-rm -rf stderr.txt
 rm -rf student-submission
 git clone $1 student-submission
+echo 'Finished cloning'
+
+cp TestListExamples.java student-submission
+cp -R lib student-submission
+
 cd student-submission
-if  ! [ -e ListExamples.java ]
 
-	then
-		echo "ListExamples.java file not found"
-		exit 1
-
-	else
-		cp ListExamples.java ./../
-		cd ..
-		javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> stderr.txt
-fi
-
-echo
-
-[ -s stderr.txt ]
-
-if [ $? -eq 0 ]
-
-then
-	echo "ListExamples.java file can't compile!"
-	exit 1
-
+if [[ -e ListExamples.java ]]
+then  
+  javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> error.txt
 else
-	echo "ListExamples.java file is compiled successfully!"
+  echo "wrong file submitted"
+  exit 1
 fi
 
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > stdout.txt
+[ -s error.txt ]
 
-echo
+if [[ $? -eq 0 ]]
+then
+  echo "compile error"
+  exit 1
+else
+  java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
+fi
 
-COUNT_FILTER_TEST=$(grep -o "ListExamples.filter(" TestListExamples.java | wc -l)
+filter_tests=$(grep -c "testFilter" TestListExamples.java)
+merge_tests=$(grep -c "testMerge" TestListExamples.java)
 
-COUNT_MERGE_TEST=$(grep -o "ListExamples.merge(" TestListExamples.java | wc -l)
+filter_failed=$(grep -c "TestListExamples.testFilter" output.txt)
+merge_failed=$(grep -c "TestListExamples.testMerge" output.txt)
 
-COUNT_FILTER=$(grep -o "testFilter" stdout.txt | wc -l)
+filter_passed=$(echo "$filter_tests-$filter_failed" | bc)
+merge_passed=$(echo "$merge_tests-$merge_failed" | bc)
 
-COUNT_MERGE=$(grep -o "testMerge" stdout.txt | wc -l)
+filter_failed_tests=$(grep " testFilter" output.txt)
+merge_failed_tests=$(grep " testMerge" output.txt)
 
-PASSED_FILTER=$(echo "$COUNT_FILTER_TEST-$COUNT_FILTER/2" | bc)
+echo "Passed $filter_passed out of $filter_tests test for filter() method!"
+echo "Failed $filter_failed out of $filter_tests test for filter() method!"
+if ! [[ filter_failed -eq 0 ]]
+then
+  echo "The failed filter tests are"
+  echo "$filter_failed_tests"
+fi
 
-PASSED_MERGE=$(echo "$COUNT_MERGE_TEST-$COUNT_MERGE/2" | bc)
-
-echo "you passed $PASSED_FILTER out of $COUNT_FILTER_TEST test for filter() method!"
-
-echo
-
-echo "you passed $PASSED_MERGE out of $COUNT_MERGE_TEST for merge() method!"
+echo "Passed $merge_passed out of $merge_tests test for merge() method!"
+echo "Failed $merge_failed out of $merge_tests test for merge() method!"
+if ! [[ merge_failed -eq 0 ]]
+then
+  echo "The failed merge tests are"
+  echo "$merge_failed_tests"
+fi
